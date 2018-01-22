87af28557c45c2ebd07f0bf4a93a757ba2f6f23f
Samuel Dotson <samdotson1992@gmail.com>
2018-01-22 13:13:34 -0600

    adjusting settings for deployment

--- GeoMarkerApp/LetterBoxApp/settings.py
+++ GeoMarkerApp/LetterBoxApp/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
 
 with open('/home/samsam/GeoMarkerParent/private/secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
