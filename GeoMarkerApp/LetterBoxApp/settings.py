e6630dea15a944f40998c06bba39ea87d560ea81
Samuel Dotson <samdotson1992@gmail.com>
2018-01-22 13:11:44 -0600

    adjusting settings for deployment

--- GeoMarkerApp/LetterBoxApp/settings.py
+++ GeoMarkerApp/LetterBoxApp/settings.py
@@ -22,6 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 
 
+with open('/home/samsam/GeoMarkerParent/private/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
